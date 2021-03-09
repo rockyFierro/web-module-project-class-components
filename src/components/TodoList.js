d94fccd23063e476fec9cb68560e946672ea4735
rockyFierro <finnslighthouse@gmail.com>
2021-03-09 12:50:41 -0600

    TodoList created

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,16 @@
+import React, { Component } from 'react';
+
+class TodoList extends React.Component{
+render(){
+  return(
+    <div>
+
+    </div>
+  )
+}
+}
+
+export default TodoList;
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
